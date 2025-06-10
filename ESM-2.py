e52be4ed18cd2f34844f42985fffa9cd178f7b35
idrugLab <58386268+idrugLab@users.noreply.github.com>
2025-06-10 15:19:39 +0800

    Update ESM-2.py

--- ESM-2.py
+++ ESM-2.py
@@ -4,15 +4,10 @@ import pandas as pd
 import pickle
 
 # Load ESM-2 model
-# model, alphabet = esm.pretrained.esm2_t6_8M_UR50D()
-model_path = "/share/home/liuchong/hjt/multimodel/model_weight/esm2_t30_150M_UR50D.pt"
-model, alphabet = esm.pretrained.load_model_and_alphabet_local(model_path)
-# model, alphabet = esm.pretrained.esm2_t36_3B_UR50D()
+# model, alphabet = esm.pretrained.load_model_and_alphabet_local(model_path)
+model, alphabet = esm.pretrained.esm2_t30_150M_UR50D()
 batch_converter = alphabet.get_batch_converter()
-model.eval()  # disables dropout for deterministic results
-
-# Prepare data (first 2 sequences from ESMStructuralSplitDataset superfamily / 4)
-
+model.eval() 
 df = pd.read_excel(r'data.xlsx',)
 
 protain = df['sequence'].tolist()
@@ -25,22 +20,18 @@ for i in range(len(protain)):
 sequence_representations = []
 
 for data in datas:
-    # print(data)
     try :
         batch_labels, batch_strs, batch_tokens = batch_converter([data])
     except Exception as e:
         print(data)
     batch_lens = (batch_tokens != alphabet.padding_idx).sum(1)
 
-    # Extract per-residue representations (on CPU)
     with torch.no_grad():
         results = model(batch_tokens, repr_layers=[30], return_contacts=True)
 
     token_representations = results["representations"][30]
 
 # Generate per-sequence representations via averaging
-# NOTE: token 0 is always a beginning-of-sequence token, so the first residue is token 1.
-
     for i, tokens_len in enumerate(batch_lens):
         sequence_representations.append(token_representations[i, 1 : tokens_len - 1].mean(0))
 esm_embedding = {}
@@ -53,8 +44,6 @@ file_path = 'Peptide.pkl'
 with open(file_path, 'wb') as file:
     pickle.dump(esm_embedding, file)
 
-# 加载保存的长字典
 with open(file_path, 'rb') as file:
     loaded_dict = pickle.load(file)
     
-# Look at the unsupervised self-attention map contact predictions
